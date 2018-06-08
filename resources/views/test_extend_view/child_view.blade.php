218b7ca5c95a9dc87ab525980fbcf66a71dc4a7a
TheThai <thethai.nguyen.212@gmail.com>
2018-06-08 19:06:47 +0700

    first commit

---
+++ resources/views/test_extend_view/child_view.blade.php
@@ -0,0 +1,8 @@
+@extends('test_extend_view.index')
+
+@section('content')
+
+<h1>hellooooooo</h1>
+
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('post.home');
+});
+
+Route::get('post', function () {
+    return view('post.post');
+});
+
+Route::get('index', function () {
+    return view('post.home');
+});
+
+Route::get('detail', function () {
+    return view('post.detail');
+});
+
+
